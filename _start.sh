372ad12f25f2e50298e5c3f8f6b4519f09ca30d7
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2020-02-17 16:04:56 +0100

    RPi Changes

--- _start.sh
+++ _start.sh
@@ -1,3 +1,3 @@
-docker build -t node .
+#docker build -t node .
 docker container rm -f bumble_automation
-docker run -dit --shm-size=1gb --name bumble_automation --restart always -v //c/Dropbox/SimpleSoftwareStudioShare/Node_BumbleAutomation/:/app node
+docker run -dit --shm-size=1gb --name bumble_automation --restart always -v /media/usb/projects/Node_BumbleAutomation/:/app node
